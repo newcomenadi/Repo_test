3f8fc76a15f2f67346eaaacca05934e164d25394
newcomenadi <30655928+newcomenadi@users.noreply.github.com>
2017-11-22 15:40:22 +0200

    Update Terminal_commands.sh
    UID stuff

--- Terminal_commands.sh
+++ Terminal_commands.sh
@@ -5,17 +5,23 @@ uname -r (linux kernel)
 uname -m (processor type)
 uname -a
 
-<<======================><======================>>Work<<======================><======================>>
+<<======================><======================>>Hydra<<======================><======================>>
 cbe NO_TESTS=1 NO_AUDIT=1 NO_ENCORE=1 make
 cbe ~/sandbox/ijidea/2017.8.24/bin/idea.sh &>/dev/null &
 
+<<======================><======================>>UID<<======================><======================>>
+cbe make -j16 && cbe make linkroot && cbe make copyimage
+cd ~/sandbox/agherca-workspace2/b-tcdclient-harmony/srcroot/sw/tvsql
+cd ~/sandbox/agherca-workspace2/b-tcdclient-harmony/srcroot/sw/npk
+cd ~/sandbox/agherca-workspace2/b-tcdclient-harmony/srcroot/sw/tivoapp
+
 <<======================><======================>>Push TcdSchema to TcdClient<<======================><======================>>
+PUSH_TIVO_SYSTEM='$$TIVO_SYSTEM'
 cd ~/sandbox/agherca-workspace2/b-tcdschema-mainline/srcroot/
 source SOURCEME.SH dev-x86
 cbe make push-script>>push2tcdclient.sh
 subl push2tcdclient.sh
-	export PUSH_TGT=~/sandbox/agherca-workspace2/b-tcdclient-harmony
-	export PUSH_TIVO_SYSTEM=dev-arm
+	#location of target roots PUSH_TGT=/.../b-tcdclient-harmony
 cbe PUSH=1 NO_TESTS=1 NO_AUDIT=1 make
 
 <<======================><======================>>Bootstrapping-InitialSetup<<======================><======================>>
